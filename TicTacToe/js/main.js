feefd017c3899ffdb27aa39afecfdbb8bee8c951
Alaa Alkatlabe <302232189@student.rocmondriaan.nl>
2021-06-24 00:36:40 +0200

    new tiktaktoe added

--- TicTacToe/js/main.js
+++ TicTacToe/js/main.js
@@ -1,47 +1,138 @@
 import Player from "./Player.js";
-
-/**
- * To start:
- * Check index.html, make sure the board with fields are created with correct classnames
- * The board should be 3x3 fields
- * Add correct classes
- */
-
-
-
-const players = [];
-let currentPlayer = 0; // This is the index of the array of the currentplayer
-const fields = document.querySelectorAll('.board > .field');
-const resetButton = document.querySelector(".reset-btn");
-
-const playerOneName = document.querySelector('.player-one-name');
-const playerTwoName = document.querySelector('.player-two-name');
-const playerOneSymbol = document.querySelector('.player-one-symbol');
-const playerTwoSymbol = document.querySelector('.player-two-symbol');
+// Constants
+const playerNameInputField = document.querySelector('.input-field-player-name');
+const addPlayerButton = document.querySelector('.add-player-button');
+const resetGameButton = document.querySelector('.reset-game-button');
 const gameStatus = document.querySelector('.game-status');
 
+let currentPlayer = 0;
+const fields = document.querySelectorAll('.tic-tac-toe-board > .box');
 
-//Create two players aligned with the Player class
-//const playerOne = ...
-const playerOne = new Player("Alaa", "X");
-//const playerTwo = ...
-const playerTwo = new Player("ALi", "O");
-//Add both players to the players array
-players.push(playerOne);
-players.push(playerTwo);
-playerOneName.textContent = players[0].name;
-playerOneSymbol.textContent = players[0].symbol;
-playerTwoSymbol.textContent = players[1].symbol;
-playerTwoName.textContent = players[1].name;
-console.log(players);
+// Elements of player one
+const playerOneLabel = document.querySelector('.player-one-label');
+const addPointsPlayerOneButton = document.querySelector('.add-points-player1-button');
+
+// Elements of player two
+const playerTwoLabel = document.querySelector('.player-two-label');
+const addPointsPlayerTwoButton = document.querySelector('.add-points-player2-button');
+
+// Array of players
+let players = [];
+
+// Starting version of the game
+resetGame();
+
+// Event listeners
+if (addPlayerButton) {
+    addPlayerButton.addEventListener("click", addPlayer);
+}
+
+if (resetGameButton) {
+    resetGameButton.addEventListener("click", resetGame);
+    resetGameButton.addEventListener("click", status);
+}
+
+if (addPointsPlayerOneButton) {
+    addPointsPlayerOneButton.addEventListener("click", addPointsPlayerOne);
+}
+
+if (addPointsPlayerTwoButton) {
+    addPointsPlayerTwoButton.addEventListener("click", addPointsPlayerTwo);
+}
+
+// Functions
+function addPlayer() {
+    if (players.length >= 2) {
+        alert("There are 2 players (or more) already. Press Reset button to start a new game.");
+        return;
+    }
+
+    const playerName = playerNameInputField.value;
+    let symbol = "X";
+    if (players.length == 1) {
+        symbol = "O";
+    }
+    const newPlayer = new Player(playerName, symbol);
+    if (playerName === "") {
+        alert("You can't play without entering a name")
+    }
+    else {
+        players.push(newPlayer);
+    }
+    printPlayers();
+}
+
+function printPlayers() {
+    playerNameInputField.value = "";
+
+    console.log("Print your players here");
+
+    if (players.length == 0) {
+        console.log("No one to play");
+        playerOneLabel.innerHTML = "Enter player one..";
+        playerTwoLabel.innerHTML = "Enter player two..";
+        return;
+    }
+
+    playerOneLabel.value
+
+    for (let i = 0; i < players.length; i++) {
+        let playersText = "";
+        let player = players[i];
+
+        playersText += "Name: " + player.name + "<br>";
+        playersText += "Symbol: " + player.symbol + "<br>";
+        playersText += "Points: " + player.points + "<br>";
+
+        console.log("Player is: " + JSON.stringify(players[i]) + JSON.stringify(playerOneLabel.points));
+
+        if (i == 0) {
+            playerOneLabel.innerHTML = playersText;
+
+            addPointsPlayerOneButton.parentElement.style.visibility = "visible";// Shows add points button
+        } else if (i == 1) {
+            playerTwoLabel.innerHTML = playersText;
+
+            addPointsPlayerTwoButton.parentElement.style.visibility = "visible";// Shows add points button
+        } else {
+            return;
+        }
+    }
+}
+
+function resetGame() {
+    console.log("Resetting the game");
+    players = [];
+    printPlayers();
+    addPointsPlayerOneButton.parentElement.classList.add('hidden');
+    addPointsPlayerTwoButton.parentElement.classList.add('hidden')
+}
+function status() {
+    setTimeout(function () { gameStatus.textContent = ""; }, 3000);
+}
+function clearField() {
+    console.log("Resetting the game");
+    for (let i = 0; i < fields.length; i++) {
+        fields[i].textContent = "";
+    }
+}
+
+function addPointsPlayerOne() {
+    console.log("Adding a point to score of player 1");
+    players[0].addPoints();
+
+    printPlayers();
+}
+
+function addPointsPlayerTwo() {
+    console.log("Adding a point to score of player 2");
+    players[1].addPoints();
+
+    printPlayers();
+}
 
 
-/**
- * Assignment
- * Make a loop thru all the fields and add a click event. 
- * Connect the addSymbolToField function in the eventHandler
- */
-//for ...
+
 for (let i = 0; i < fields.length; i++) {
     fields[i].addEventListener('click', function () {
         addSymbolToField(this);
@@ -52,27 +143,11 @@ for (let i = 0; i < fields.length; i++) {
 
 }
 
-
-/**
- * Assignment 
- * Give body to the reset function (the function exists below)
- */
-resetButton.addEventListener("click", resetGame);
-resetButton.addEventListener("click", status);
-
-
-
 function addSymbolToField(field) {
     const fieldContent = field.textContent;
     if (fieldContent === 'X' || fieldContent === 'O') {
         alert('This field can not be used');
     }
-
-    /**
-     * Assignment
-     * Add the current player symbol to the field textContent
-     * What more needs to be done here? Make a short todolist
-     */
     field.textContent = players[currentPlayer].symbol;
     currentPlayer++;
     if (currentPlayer > 1) {
@@ -92,13 +167,6 @@ const winningConditions = [
     [2, 4, 6]
 ];
 function checkWinner() {
-    /**
-     * Assignment
-     * Add an algorithm to check if someone has three in a row
-     * Try to use Internet - if not found the teacher will give you one
-     * Also make sure you check for a draw (gelijkspel)
-     * Again what more needs to be done, make a short todolist
-     */
     for (let i = 0; i < winningConditions.length; i++) {
         const winline = winningConditions[i];
         let winX = true;
@@ -121,9 +189,9 @@ function checkWinner() {
             addPointsPlayerOne();
             gameStatus.textContent = "";
             const p = document.createElement("p");
-            p.textContent += players[0].name + " " + players[0].symbol + " " + "Win!" + " " + "score of player " + players[0].getPoints();
+            p.textContent = players[0].name + " " + "Win!";
             gameStatus.append(p);
-            setTimeout(resetGame, 2000);
+            setTimeout(clearField, 2000);
             status();
         }
         else if (winO) {
@@ -131,9 +199,9 @@ function checkWinner() {
             addPointsPlayerTwo();
             gameStatus.textContent += "";
             const p = document.createElement("p");
-            p.textContent += players[1].name + " " + players[1].symbol + " " + "Win!" + " " + "score of player " + players[1].getPoints();
+            p.textContent = players[1].name + " " + "Win!";
             gameStatus.append(p);
-            setTimeout(resetGame, 2000);
+            setTimeout(clearField, 2000);
             status();
         }
         else if ((fields[0].textContent == 'X' || fields[0].textContent == 'O') && (fields[1].textContent == 'X'
@@ -146,37 +214,12 @@ function checkWinner() {
             const p = document.createElement("p");
             p.textContent = "Draw";
             gameStatus.append(p);
-            setTimeout(resetGame, 2000);
+            setTimeout(clearField, 2000);
             status();
         }
 
     }
 
-}
-
-function resetGame() {
-    /**
-     * Assignment
-     * Make sure this works (all fields empty, reset data if needed)
-     */
-    console.log("Resetting the game");
-    for (let i = 0; i < fields.length; i++) {
-        fields[i].textContent = "";
-    }
-
-}
-function status() {
-    setTimeout(function () { gameStatus.textContent = ""; }, 10000);
-}
-
-function addPointsPlayerOne() {
-    console.log(" score of player 1");
-    players[0].addPoints();
-
-}
-function addPointsPlayerTwo() {
-    console.log(" score of player 2");
-    players[1].addPoints();
 }
 
 
