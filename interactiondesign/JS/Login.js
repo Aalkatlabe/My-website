254f649279b0667c2b8290cc73b27785d5705494
Alaa Alkatlabe <302232189@student.rocmondriaan.nl>
2021-06-27 23:04:10 +0200

    Js en html interactiondesign fixed

--- interactiondesign/JS/Login.js
+++ interactiondesign/JS/Login.js
@@ -9,8 +9,10 @@ const studenten = [];
 
 const student1 = new Student("Outmane", "302319425@student.rocmondriaan.nl", "0000");
 const student2 = new Student("Alaa", "30223328@student.rocmondriaan.nl", "9999");
+const student3 = new Student("Zakaria", "302586168@student.rocmondriaan.nl", "zzzz");
 studenten.push(student1);
 studenten.push(student2);
+studenten.push(student3);
 
 
 loginBtn.addEventListener('click', function () {
